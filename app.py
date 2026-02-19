f2e86ec5404e72e2b04b02b131d526dc349fb4fe
Abhiroop Agarwal <abhiroop@Abhiroops-MacBook-Pro.local>
2026-02-19 19:19:56 +0530

    Fix launch error: use ssr_mode=False instead of ssr=False

--- app.py
+++ app.py
@@ -687,7 +687,7 @@ if __name__ == "__main__":
     app.launch(
         server_name="0.0.0.0", 
         server_port=7860, 
-        ssr=False, 
+        ssr_mode=False, 
         show_api=False
     )
 
